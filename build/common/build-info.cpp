8075bc3a130549ba48f8e4373c2fb1b0dd170d3e
narugupta <narugupta01@gmail.com>
2026-05-27 05:44:36 +0000

    clean repo: server only, removed unused tools, examples, pocs, app

--- build/common/build-info.cpp
+++ build/common/build-info.cpp
@@ -3,8 +3,8 @@
 #include <cstdio>
 #include <string>
 
-int LLAMA_BUILD_NUMBER = 0;
-char const * LLAMA_COMMIT = "unknown";
+int LLAMA_BUILD_NUMBER = 1;
+char const * LLAMA_COMMIT = "6b1fe3b";
 char const * LLAMA_COMPILER = "GNU 13.3.0";
 char const * LLAMA_BUILD_TARGET = "Linux x86_64";
 
